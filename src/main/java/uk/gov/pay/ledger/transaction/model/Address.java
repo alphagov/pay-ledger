7c4767e0cd2acfbf6e0485a9d1484f5a3f347792
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2019-06-13 12:01:19 +0100

    PP-5256 match CardDetails to that of connector
    - match the JSON format of CardDetails to connector

--- src/main/java/uk/gov/pay/ledger/transaction/model/Address.java
+++ src/main/java/uk/gov/pay/ledger/transaction/model/Address.java
@@ -1,9 +1,9 @@
 package uk.gov.pay.ledger.transaction.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Address {
 
     private String addressLine1;
