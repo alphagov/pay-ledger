75bc583389af6af5a5b587a2ec5dcf424152c73f
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-06-28 13:39:11 +0100

    Refactors to use a NameValuePair

--- src/main/java/uk/gov/pay/ledger/healthcheck/SQSHealthCheck.java
+++ src/main/java/uk/gov/pay/ledger/healthcheck/SQSHealthCheck.java
@@ -6,12 +6,13 @@ import com.amazonaws.services.sqs.model.AmazonSQSException;
 import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
 import com.codahale.metrics.health.HealthCheck;
 import com.google.inject.Inject;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.pay.ledger.app.LedgerConfig;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Optional;
 
 import static java.lang.String.format;
@@ -20,51 +21,43 @@ public class SQSHealthCheck extends HealthCheck {
 
     private final AmazonSQS sqsClient;
     private final Logger logger = LoggerFactory.getLogger(SQSHealthCheck.class);
-    private List<SqsHealthCheckQueueHolder> checkList = new ArrayList<>();
+    private NameValuePair sqsHealthCheckHolder;
 
     @Inject
     public SQSHealthCheck(AmazonSQS sqsClient, LedgerConfig ledgerConfig) {
         this.sqsClient = sqsClient;
-        setUpCheckList(ledgerConfig);
+        setUpSQSHealthCheckHolder(ledgerConfig);
+    }
+
+    private void setUpSQSHealthCheckHolder(LedgerConfig ledgerConfig) {
+        this.sqsHealthCheckHolder = new BasicNameValuePair("event", ledgerConfig.getSqsConfig().getEventQueueUrl());
     }
 
     @Override
     protected Result check() throws Exception {
-        for (SqsHealthCheckQueueHolder checkValues : checkList) {
-            Optional<String> result = checkQueue(checkValues.queueUrl);
-            if (result.isPresent()) {
-                return Result.unhealthy(format("failed to retrieve %s queue attributes: %s", checkValues.queueIdentifier, result.get()));
-            }
+        Optional<String> result = checkQueue(sqsHealthCheckHolder);
+
+        if (result.isPresent()) {
+            return Result.unhealthy(format("failed to retrieve %s queue attributes: %s", sqsHealthCheckHolder.getName(), result.get()));
         }
 
         return Result.healthy();
     }
 
-    private void setUpCheckList(LedgerConfig ledgerConfig) {
-        checkList.add(new SqsHealthCheckQueueHolder(ledgerConfig.getSqsConfig().getEventQueueUrl(), "event"));
-    }
-
-    private Optional<String> checkQueue(String queueUrl) {
-        GetQueueAttributesRequest queueAttributesRequest = new GetQueueAttributesRequest(queueUrl).withAttributeNames("All");
+    private Optional<String> checkQueue(NameValuePair nameValuePair) {
+        GetQueueAttributesRequest queueAttributesRequest =
+                new GetQueueAttributesRequest(nameValuePair.getValue())
+                        .withAttributeNames("All");
         try {
             sqsClient.getQueueAttributes(queueAttributesRequest);
         } catch (AmazonSQSException | UnsupportedOperationException e) {
-            logger.error("Failed to retrieve queue attributes - {}", e.getMessage());
+            logger.error("Failed to retrieve [{}] queue attributes - {}", nameValuePair.getName(), e.getMessage());
             return Optional.of(e.getMessage());
         } catch (SdkClientException e) {
             logger.error("Failed to connect to sqs queue - {}", e.getMessage());
+            return Optional.of("Failed to connect to sqs server - " + e.getMessage());
         }
 
         return Optional.empty();
-    }
-
-    private class SqsHealthCheckQueueHolder {
-        private String queueUrl;
-        private String queueIdentifier;
-
-        public SqsHealthCheckQueueHolder(String queueUrl, String queueIdentifier) {
-            this.queueUrl = queueUrl;
-            this.queueIdentifier = queueIdentifier;
-        }
     }
 }
