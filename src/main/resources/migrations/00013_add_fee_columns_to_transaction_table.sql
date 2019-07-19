5d1ef326895369bc612b1967164b3686a64b62ef
Maria Jankowiak <maria.jankowiak@digital.cabinet-office.gov.uk>
2019-07-19 13:51:06 +0100

    PP-5460: remove corporate_surcharge column

--- src/main/resources/migrations/00013_add_fee_columns_to_transaction_table.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset uk.gov.pay:add_fee_columns_to_transaction_table
-
-ALTER TABLE transaction
-    ADD COLUMN fee BIGINT,
-    ADD COLUMN corporate_surcharge BIGINT;
