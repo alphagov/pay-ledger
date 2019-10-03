26a41c9019b15f14991aa456ab65ea425743cc23
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-10-03 15:15:43 +0100

    PP-5670: Specify the consumer when running contract test
    Currently when a consumer build is kicked off, the provider contract test will
    run in its entirety. For example, in a selfservice build, the ledger provider
    contract tests will run as well. However, what is run is:
    
    publicapi->ledger
    selfservice->ledger
    connector->ledger
    
    We really only want to run selfservice->ledger in this case.
    
    This commit will allow a dynamic creation of the test suite to run based on a
    CONSUMER system property. This will mean shorter run times when a provider
    contract test is run as part of a specific consumer build.

--- src/test/java/uk/gov/pay/ledger/pact/ContractTestSuite.java
+++ src/test/java/uk/gov/pay/ledger/pact/ContractTestSuite.java
@@ -1,20 +1,27 @@
 package uk.gov.pay.ledger.pact;
 
+import com.google.common.collect.ImmutableSetMultimap;
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.runners.AllTests;
+import uk.gov.pay.commons.testing.pact.provider.CreateTestSuite;
 
-@RunWith(Suite.class)
-
-@Suite.SuiteClasses({
-        TransactionsAndEventApiContractTest.class,
-        PaymentCreatedEventQueueContractTest.class,
-        PaymentDetailsEnteredEventQueueContractTest.class,
-        CaptureConfirmedEventQueueContractTest.class,
-        RefundCreatedByUserEventQueueContractTest.class,
-        RefundSubmittedEventQueueContractTest.class,
-        RefundSucceededEventQueueContractTest.class,
-        CaptureSubmittedEventQueueContractTest.class
-})
+@RunWith(AllTests.class)
 public class ContractTestSuite {
+
+    public static TestSuite suite() {
+        ImmutableSetMultimap.Builder<String, JUnit4TestAdapter> consumerToJUnitTest = ImmutableSetMultimap.builder();
+        consumerToJUnitTest.put("publicapi", new JUnit4TestAdapter(PublicApiContractTest.class));
+        consumerToJUnitTest.put("selfservice", new JUnit4TestAdapter(SelfServiceContractTest.class));
+        consumerToJUnitTest.put("connector", new JUnit4TestAdapter(PaymentCreatedEventQueueContractTest.class));
+        consumerToJUnitTest.put("connector", new JUnit4TestAdapter(PaymentDetailsEnteredEventQueueContractTest.class));
+        consumerToJUnitTest.put("connector", new JUnit4TestAdapter(CaptureConfirmedEventQueueContractTest.class));
+        consumerToJUnitTest.put("connector", new JUnit4TestAdapter(RefundCreatedByUserEventQueueContractTest.class));
+        consumerToJUnitTest.put("connector", new JUnit4TestAdapter(RefundSubmittedEventQueueContractTest.class));
+        consumerToJUnitTest.put("connector", new JUnit4TestAdapter(RefundSucceededEventQueueContractTest.class));
+        consumerToJUnitTest.put("connector", new JUnit4TestAdapter(CaptureSubmittedEventQueueContractTest.class));
+        return CreateTestSuite.create(consumerToJUnitTest.build());
+    }
 
 }
