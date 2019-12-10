d96747df7209dcb3febc6d9ae67222f0524a3031
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-12-10 13:33:30 +0000

    PP-5893-Add-MDC-Logging-Filter
    Description:
    - At the moment this doesn't compile as Pay Java Commons doesn't have the static members PARENT_EXTERNAL_ID and TRANSACTION_EXTERNAL_ID
    - Adds eventId, parent_external_id and transactionExternalId to MDC

---
+++ src/main/java/uk/gov/pay/ledger/filters/LoggingMDCRequestFilter.java
@@ -0,0 +1,32 @@
+package uk.gov.pay.ledger.filters;
+
+import org.slf4j.MDC;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import java.io.IOException;
+import java.util.Optional;
+
+import static uk.gov.pay.logging.LoggingKeys.LEDGER_EVENT_ID;
+
+public class LoggingMDCRequestFilter implements ContainerRequestFilter {
+
+    public static final String PARENT_TRANSACTION_EXTERNAL_ID = "parent_transaction_external_id";
+    public static final String TRANSACTION_EXTERNAL_ID = "transaction_external_id";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        getPathParameterFromRequest("eventId", requestContext)
+                .ifPresent(eventId -> MDC.put(LEDGER_EVENT_ID, eventId));
+
+        getPathParameterFromRequest("transactionExternalId", requestContext)
+                .ifPresent(transactionExternalId -> MDC.put(TRANSACTION_EXTERNAL_ID, transactionExternalId));
+
+        getPathParameterFromRequest("parentTransactionExternalId", requestContext)
+                .ifPresent(parentTransactionExternalId -> MDC.put(PARENT_TRANSACTION_EXTERNAL_ID, parentTransactionExternalId));
+    }
+
+    private Optional<String> getPathParameterFromRequest(String parameterName, ContainerRequestContext requestContext) {
+        return Optional.ofNullable(requestContext.getUriInfo().getPathParameters().getFirst(parameterName));
+    }
+}
