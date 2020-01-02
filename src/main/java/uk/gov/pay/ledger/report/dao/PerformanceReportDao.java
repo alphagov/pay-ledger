6a2d056b3f99f579e11b89cdced821a08593f2cb
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2020-01-02 11:08:50 +0000

    PP-5978: Performance report by date range

--- src/main/java/uk/gov/pay/ledger/report/dao/PerformanceReportDao.java
+++ src/main/java/uk/gov/pay/ledger/report/dao/PerformanceReportDao.java
@@ -5,13 +5,17 @@ import uk.gov.pay.ledger.report.mapper.PerformanceReportEntityMapper;
 import uk.gov.pay.ledger.report.entity.PerformanceReportEntity;
 
 import javax.inject.Inject;
+import java.time.ZonedDateTime;
 
 public class PerformanceReportDao {
 
     private static final String PERFORMANCE_REPORT =
             "select coalesce(count(amount),0) as volume, coalesce(sum(amount),0) total_amount, " +
                     "coalesce(avg(amount),0) avg_amount from transaction " +
-            "where state='SUCCESS' and type='PAYMENT' AND live=true";
+            "where state='SUCCESS' and type='PAYMENT' and live=true";
+
+    private static final String PERFORMANCE_REPORT_WITH_DATE_RANGE =
+            PERFORMANCE_REPORT + " and created_date between :startDate and :toDate";
 
     private final Jdbi jdbi;
 
@@ -23,6 +27,14 @@ public class PerformanceReportDao {
     public PerformanceReportEntity performanceReportForPaymentTransactions() {
         return jdbi.withHandle(handle ->
                 handle.createQuery(PERFORMANCE_REPORT)
+                        .map(new PerformanceReportEntityMapper()).findOnly());
+    }
+
+    public PerformanceReportEntity performanceReportForPaymentTransactions(ZonedDateTime fromDate, ZonedDateTime toDate) {
+        return jdbi.withHandle(handle ->
+                handle.createQuery(PERFORMANCE_REPORT_WITH_DATE_RANGE)
+                        .bind("startDate", fromDate)
+                        .bind("toDate", toDate)
                         .map(new PerformanceReportEntityMapper()).findOnly());
     }
 }
