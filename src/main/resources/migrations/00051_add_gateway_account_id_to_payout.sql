e50a001a66cdf781c9b1e77c72ee221cf57389a3
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2020-05-18 11:41:26 +0100

    PP-6502 add gateway_account_id to payouts table
    - add new column gateway_account_id VARCHAR(255) to payouts table
    - add index on gateway_account_id

---
+++ src/main/resources/migrations/00051_add_gateway_account_id_to_payout.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+--changeset uk.gov.pay:add_gateway_account_id_to_payout
+ALTER TABLE payout ADD COLUMN gateway_account_id VARCHAR(255);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS payout_gateway_account_id_idx ON payout USING btree(gateway_account_id);
+
+--rollback ALTER TABLE payouts DROP COLUMN gateway_account_id;
