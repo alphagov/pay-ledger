762cb117d7f9a2841c688a04428dc8bffbbd3101
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2021-03-03 14:10:03 +0000

    PP-7832 add upsert method to TransactionMetadataDao
    - add a new method to upsert values on transaction_metada table
    - add relevant test

--- src/main/java/uk/gov/pay/ledger/transactionmetadata/dao/TransactionMetadataDao.java
+++ src/main/java/uk/gov/pay/ledger/transactionmetadata/dao/TransactionMetadataDao.java
@@ -36,11 +36,27 @@ public class TransactionMetadataDao {
             "    WHERE metadata_key_id in (select id from metadata_key where key = :key ) " +
             "      and transaction_id = :transactionId)";
 
+    private static final String UPSERT_STRING = "INSERT INTO transaction_metadata(transaction_id, metadata_key_id, value) " +
+            "VALUES(:transactionId, (select id from metadata_key where key = :key ), :value) " +
+            "ON CONFLICT ON CONSTRAINT  transaction_id_and_metadata_key_id_key DO UPDATE SET "+
+            "value = EXCLUDED.value WHERE transaction_metadata.transaction_id = EXCLUDED.transaction_id AND " +
+            "transaction_metadata.metadata_key_id = EXCLUDED.metadata_key_id";
+
     private final Jdbi jdbi;
 
     @Inject
     public TransactionMetadataDao(Jdbi jdbi) {
         this.jdbi = jdbi;
+    }
+
+    public void upsert(Long transactionId, String key, String value) {
+        jdbi.withHandle(handle ->
+                handle.createUpdate(UPSERT_STRING)
+                        .bind("transactionId", transactionId)
+                        .bind("key", key)
+                        .bind("value", value)
+                        .execute()
+        );
     }
 
     public void insertIfNotExist(Long transactionId,
