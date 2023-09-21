0427c4102d9052629b2e84a89744cfabf1aeee57
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2023-09-21 22:01:00 +0100

    PP-11232 Update service to redact PII from transactions
    - Redacts PII from transactions and deletes related events
    - redact PII.. method processes transactions for a date range
        - start date: created_date of last processed transaction. If not gets the first transaction created date. When there are no transactions, uses the derived date up to which transactions can be redacted based on config.
        - end date: Date up to which transactions can be redacted. This is based on config (default is now() - 7 years)
    - Uses the system clock provided so fixed dates can be used for testing

--- src/main/java/uk/gov/pay/ledger/expungeorredact/service/ExpungeOrRedactService.java
+++ src/main/java/uk/gov/pay/ledger/expungeorredact/service/ExpungeOrRedactService.java
@@ -6,16 +6,33 @@ import org.slf4j.LoggerFactory;
 import uk.gov.pay.ledger.app.LedgerConfig;
 import uk.gov.pay.ledger.app.config.ExpungeOrRedactHistoricalDataConfig;
 import uk.gov.pay.ledger.event.dao.EventDao;
+import uk.gov.pay.ledger.expungeorredact.dao.TransactionRedactionInfoDao;
 import uk.gov.pay.ledger.transaction.dao.TransactionDao;
+import uk.gov.pay.ledger.transaction.entity.TransactionEntity;
 
 import javax.inject.Inject;
+import java.time.Clock;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.min;
+import static java.time.ZoneOffset.UTC;
+import static java.util.Optional.ofNullable;
+import static net.logstash.logback.argument.StructuredArguments.kv;
+import static uk.gov.service.payments.logging.LoggingKeys.RESOURCE_EXTERNAL_ID;
 
 public class ExpungeOrRedactService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ExpungeOrRedactService.class);
+    private final TransactionRedactionInfoDao transactionRedactionInfoDao;
     private final ExpungeOrRedactHistoricalDataConfig expungeOrRedactHistoricalDataConfig;
-    private TransactionDao transactionDao;
-    private EventDao eventDao;
+    private final Clock clock;
+    private final TransactionDao transactionDao;
+    private final EventDao eventDao;
+
+    private static final int PAGE_SIZE = 500;
 
     private static final Histogram duration = Histogram.build()
             .name("expunge_and_redact_historical_data_job_duration_seconds")
@@ -23,16 +40,23 @@ public class ExpungeOrRedactService {
             .register();
 
     @Inject
-    public ExpungeOrRedactService(TransactionDao transactionDao, EventDao eventDao, LedgerConfig ledgerConfig) {
+    public ExpungeOrRedactService(TransactionDao transactionDao, EventDao eventDao,
+                                  TransactionRedactionInfoDao transactionRedactionInfoDao,
+                                  LedgerConfig ledgerConfig,
+                                  Clock clock) {
         this.transactionDao = transactionDao;
         this.eventDao = eventDao;
+        this.transactionRedactionInfoDao = transactionRedactionInfoDao;
         this.expungeOrRedactHistoricalDataConfig = ledgerConfig.getExpungeOrRedactHistoricalDataConfig();
+        this.clock = clock;
     }
 
     public void redactOrDeleteData() {
         Histogram.Timer responseTimeTimer = duration.startTimer();
         try {
-            if (!expungeOrRedactHistoricalDataConfig.isExpungeAndRedactHistoricalDataEnabled()) {
+            if (expungeOrRedactHistoricalDataConfig.isExpungeAndRedactHistoricalDataEnabled()) {
+                redactPIIFromTransactionsAndDeleteRelatedEvents();
+            } else {
                 LOGGER.info("Expunging and redacting historical data is not enabled");
             }
         } finally {
@@ -40,4 +64,70 @@ public class ExpungeOrRedactService {
         }
     }
 
+    private void redactPIIFromTransactionsAndDeleteRelatedEvents() {
+        int totalNoOfTransactionsToRedact = expungeOrRedactHistoricalDataConfig.getNoOfTransactionsToRedact();
+        ZonedDateTime createdDateOfLastProcessedTransaction = getCreatedDateOfLastProcessedTransaction();
+        ZonedDateTime redactTransactionsUpToDate = getRedactTransactionsUpToDate();
+
+        int noOfTxsProcessed = 0;
+        int noOfEventsDeleted = 0;
+
+        while (noOfTxsProcessed < totalNoOfTransactionsToRedact) {
+            int remainingNoOfTransactions = totalNoOfTransactionsToRedact - noOfTxsProcessed;
+            int noOfTxsToFetch = min(remainingNoOfTransactions, PAGE_SIZE);
+
+            List<TransactionEntity> transactionsForRedaction = transactionDao.findTransactionsForRedaction(
+                    createdDateOfLastProcessedTransaction, redactTransactionsUpToDate, noOfTxsToFetch);
+
+            if (transactionsForRedaction.isEmpty()) {
+                break;
+            }
+
+            transactionsForRedaction.forEach(transactionEntity -> {
+                transactionDao.redactPIIFromTransaction(transactionEntity.getExternalId());
+                LOGGER.info("Redacted PII from transaction [{}]", kv(RESOURCE_EXTERNAL_ID, transactionEntity.getExternalId()));
+            });
+
+            List<String> transactionExternalIds = transactionsForRedaction
+                    .stream()
+                    .map(TransactionEntity::getExternalId)
+                    .collect(Collectors.toList());
+
+            noOfEventsDeleted += eventDao.deleteEventsForTransactions(transactionExternalIds);
+            noOfTxsProcessed += noOfTxsToFetch;
+
+            createdDateOfLastProcessedTransaction = transactionsForRedaction
+                    .stream().map(TransactionEntity::getCreatedDate)
+                    .max(ZonedDateTime::compareTo).get();
+        }
+
+        LOGGER.info("Redacted PII from transactions [{}, {}]",
+                kv("no_of_transactions_redacted", noOfTxsProcessed),
+                kv("no_of_events_deleted", noOfEventsDeleted));
+
+        if (noOfTxsProcessed > 0) {
+            transactionRedactionInfoDao.update(createdDateOfLastProcessedTransaction);
+        }
+    }
+
+    private ZonedDateTime getRedactTransactionsUpToDate() {
+        return clock.instant()
+                .minus(expungeOrRedactHistoricalDataConfig.getExpungeOrRedactDataOlderThanDays(), ChronoUnit.DAYS)
+                .atZone(UTC);
+    }
+
+    private ZonedDateTime getCreatedDateOfLastProcessedTransaction() {
+        return ofNullable(transactionRedactionInfoDao.getCreatedDateOfLastProcessedTransaction())
+                .orElseGet(this::getCreatedDateOfFirstTransaction)
+                .withZoneSameInstant(UTC);
+    }
+
+    private ZonedDateTime getCreatedDateOfFirstTransaction() {
+        ZonedDateTime dateOfFirstTransaction = transactionDao.getCreatedDateOfFirstTransaction()
+                .orElseGet(this::getRedactTransactionsUpToDate);
+
+        transactionRedactionInfoDao.insert(dateOfFirstTransaction);
+
+        return dateOfFirstTransaction;
+    }
 }
